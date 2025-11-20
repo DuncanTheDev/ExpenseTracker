0b1fe61341ae1191174ba7cdf710f590770f680c
Patrick Duncan BUENAVENTURA <Patrick.BUENAVENTURA@student.tafesa.edu.au>
2025-11-20 16:02:54 +0800

    feat: implement ProtectedRoute component and update routing in App for protected access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,42 @@
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute/Protected";
+
 import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Transaction from "./pages/Transaction/Transaction";
+import Category from "./pages/Category/Category";
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/transactions"
+          element={
+            <ProtectedRoute>
+              <Transaction />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/categories"
+          element={
+            <ProtectedRoute>
+              <Category />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
