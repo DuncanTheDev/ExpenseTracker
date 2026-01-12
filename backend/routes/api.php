ed7c3510b6c6e1ace9b851b1054dd8e5ee0cc465
Patrick Duncan BUENAVENTURA <Patrick.BUENAVENTURA@student.tafesa.edu.au>
2026-01-12 10:42:14 +0800

    Add report routes for monthly income/expense, balance trend, and category breakdown

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\TransactionController;
 
 // Route::get('/user', function (Request $request) {
@@ -15,7 +16,9 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
+    Route::get('/account', [AuthController::class, 'getUser']);
+    Route::delete('/delete', [AuthController::class, 'deleteAccount']);
+
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::post('/category', [CategoryController::class, 'store']);
     Route::put('/category/{id}', [CategoryController::class, 'update']);
@@ -25,4 +28,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/transaction', [TransactionController::class, 'store']);
     Route::put('/transaction/{id}', [TransactionController::class, 'update']);
     Route::delete('/transaction/{id}', [TransactionController::class, 'destroy']);
+
+    Route::get('/report/monthly', [ReportController::class, 'monthlyIncomeExpense']);
+    Route::get('/report/balance-trend', [ReportController::class, 'balanceTrend']);
+    Route::get('/report/category-breakdown', [ReportController::class, 'categoryBreakdown']);
 });
